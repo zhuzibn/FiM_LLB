be888a3213c0915c500922a267efdec744f22f98
Zhifeng Zhu <zzfmvp@gmail.com>
2017-06-17 15:14:39 +0800

    modify spin torque part in LLB

--- heffcalc_LLB.m
+++ heffcalc_LLB.m
@@ -20,8 +20,8 @@
 %19. addSOT, same as addSTT
 %20. thetaSHE, [unitless], double, spin hall angle, 
 %output
-function [Gam_parall_TM,Gam_parall_RE,Gam_perp_TM,Gam_perp_RE,HTM_MFA,...
-    HRE_MFA,m0_TM,m0_RE]=heffcalc_LLB(D,muRE,muTM,mRE,mTM,mTTM,mTRE,...
+function [Gam_parall_TM,Gam_parall_RE,Gam_perp_TM,Gam_perp_RE,HTM_MFA0,...
+    HRE_MFA0,m0_TM,m0_RE]=heffcalc_LLB(D,muRE,muTM,mRE,mTM,mTTM,mTRE,...
     Hext,MsT,ita,PFL,Jc,elev,tFL,J0RERE,J0TMTM,J0RETM,J0TMRE,alp,ip,bbeta,addSTT,addSOT,thetaSHE)
 constantfile();
 HARE=[0,0,2*D/muRE*mRE(3)];
@@ -54,25 +54,31 @@ Hi=[Hitmp(1),Hitmp(2),Hitmp(3)];
 end
 HRE_MFA=(muRE*HeffRE+J0RERE*mRE+J0RETM*mTM+muRE*Hi.*ip/alp)/muRE;%eqn5
 HTM_MFA=(muTM*HeffTM+J0TMTM*mTM+J0TMRE*mRE+muTM*Hi.*ip/alp)/muTM;%eqn6
+HRE_MFA0=(muRE*HeffRE+J0RERE*mRE+J0RETM*mTM)/muRE;%eqn5
+HTM_MFA0=(muTM*HeffTM+J0TMTM*mTM+J0TMRE*mRE)/muTM;%eqn6
 
 xi0_TM=bbeta*muTM*HTM_MFA;%eqn5
 xi0_RE=bbeta*muRE*HRE_MFA;
-
+xi0_TM0=bbeta*muTM*HTM_MFA0;%eqn5
+xi0_RE0=bbeta*muRE*HRE_MFA0;
 % [Bri_val_TM,Bri_prime_TM]=Bri_func(norm(xi0_TM),JFe);
 % [Bri_val_RE,Bri_prime_RE]=Bri_func(norm(xi0_RE),JGd);
-[Bri_val_TM,Bri_prime_TM]=Lang_fun(norm(xi0_TM));
-[Bri_val_RE,Bri_prime_RE]=Lang_fun(norm(xi0_RE));
-
+[Bri_val_TM,~]=Lang_fun(norm(xi0_TM));
+[Bri_val_RE,~]=Lang_fun(norm(xi0_RE));
+[Bri_val_TM0,Bri_prime_TM0]=Lang_fun(norm(xi0_TM0));
+[Bri_val_RE0,Bri_prime_RE0]=Lang_fun(norm(xi0_RE0));
 
 m0_TM=Bri_val_TM*xi0_TM/norm(xi0_TM);%eqn5
 m0_RE=Bri_val_RE*xi0_RE/norm(xi0_RE);
+% m0_TM0=Bri_val_TM0*xi0_TM0/norm(xi0_TM0);%eqn5
+% m0_RE0=Bri_val_RE0*xi0_RE0/norm(xi0_RE0);
 
 Lambda_TM=2*gam*alp/(bbeta*muTM);%eqn.6
 Lambda_RE=2*gam*alp/(bbeta*muRE);
 
-Gam_parall_TM=Lambda_TM*Bri_val_TM/(norm(xi0_TM)*Bri_prime_TM);
-Gam_parall_RE=Lambda_RE*Bri_val_RE/(norm(xi0_RE)*Bri_prime_RE);
+Gam_parall_TM=Lambda_TM*Bri_val_TM0/(norm(xi0_TM0)*Bri_prime_TM0);
+Gam_parall_RE=Lambda_RE*Bri_val_RE0/(norm(xi0_RE0)*Bri_prime_RE0);
 
-Gam_perp_TM=Lambda_TM/2*(norm(xi0_TM)/Bri_val_TM-1);
-Gam_perp_RE=Lambda_RE/2*(norm(xi0_RE)/Bri_val_RE-1);
+Gam_perp_TM=Lambda_TM/2*(norm(xi0_TM0)/Bri_val_TM0-1);
+Gam_perp_RE=Lambda_RE/2*(norm(xi0_RE0)/Bri_val_RE0-1);
 end
